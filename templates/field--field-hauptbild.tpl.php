5a68a1d776a2d32835bf474cbfcbed5aa9b4b372
Korbinian Polk <korbinianpolk@googlemail.com>
2013-05-13 16:27:00 +0200

    thumbnav styles

--- templates/field--field-hauptbild.tpl.php
+++ templates/field--field-hauptbild.tpl.php
@@ -1,5 +1,5 @@
 <?php foreach ($items as $delta => $item): ?>
-  <li data-thumb="<?php print render($item); ?>">
+  <li data-thumb="http://vu2160.s1.constructdoors.de/sites/default/files/AZ4_gr.jpg">
     <?php print render($item); ?>
   </li>
 <?php endforeach; ?>
