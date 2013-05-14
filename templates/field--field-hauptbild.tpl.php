d546bc208599257e0746b1d8a7722ad85c74144f
Korbinian Polk <korbinianpolk@googlemail.com>
2013-05-14 12:48:41 +0200

    deleted basepath

--- templates/field--field-hauptbild.tpl.php
+++ templates/field--field-hauptbild.tpl.php
@@ -1,7 +1,7 @@
 <?php foreach ($items as $delta => $item): ?>
 <!--	<?php print_r ($item); ?> -->
 
-	<?= "base" . $base_path ?>
+<!--	<?= "base" . $base_path ?> -->
   <li data-thumb="/sites/default/files/<?php print $item['#item']['filename']?>">
     <?php print render($item); ?>
   </li>
